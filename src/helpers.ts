4470ee28ccfaa8b34430d0dd890a834225f3fc1f
don <donpark@pm.me>
2021-12-22 15:45:54 -0600

    initial indexed events with synthetix guard

---
+++ src/helpers.ts
@@ -0,0 +1,101 @@
+import {
+  log,
+  Address,
+  BigInt,
+  BigDecimal,
+  ethereum,
+} from '@graphprotocol/graph-ts';
+
+import { ERC20 } from '../generated/templates/PoolLogic/ERC20';
+// import { ERC20NameBytes } from '../generated/templates/PoolLogic/ERC20NameBytes';
+import { PoolLogic } from '../generated/templates/PoolLogic/PoolLogic';
+import { PoolManagerLogic } from '../generated/templates/PoolLogic/PoolManagerLogic';
+import { Pool } from '../generated/schema';
+
+export let ZERO_BI = BigInt.fromI32(0);
+export let ONE_BI = BigInt.fromI32(1);
+export let ZERO_BD = BigDecimal.fromString('0');
+export let BI_18 = BigInt.fromI32(18);
+
+export function fetchTokenDecimals(tokenAddress: Address): BigInt {
+  let contract = ERC20.bind(tokenAddress);
+
+  // let decimalValue = null;
+  let decimalValue = NaN;
+  let decimalResult = contract.try_decimals();
+  if (!decimalResult.reverted) {
+    decimalValue = decimalResult.value;
+  }
+  return BigInt.fromI32(decimalValue as i32);
+}
+
+export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
+  let bd = BigDecimal.fromString('1');
+  for (let i = ZERO_BI; i.lt(decimals as BigInt); i = i.plus(ONE_BI)) {
+    bd = bd.times(BigDecimal.fromString('10'));
+  }
+  return bd;
+}
+
+export function convertTokenToDecimal(
+  tokenAmount: BigInt,
+  exchangeDecimals: BigInt
+): BigDecimal {
+  if (exchangeDecimals == ZERO_BI) {
+    return tokenAmount.toBigDecimal();
+  }
+  return tokenAmount.toBigDecimal().div(exponentToBigDecimal(exchangeDecimals));
+}
+
+export function instantiatePool(
+  id: string,
+  fundAddress: Address,
+  event: ethereum.Event
+): Pool {
+  let pool = Pool.load(id);
+  let poolContract = PoolLogic.bind(fundAddress);
+  let poolTokenDecimals = fetchTokenDecimals(fundAddress);
+
+  if (!pool) {
+    pool = new Pool(id);
+    pool.fundAddress = fundAddress;
+  }
+
+  // Manager Logic
+  let managerAddress = poolContract.poolManagerLogic();
+  let managerContract = PoolManagerLogic.bind(managerAddress);
+
+  // Pool Entity
+  let tryPoolName = poolContract.try_name();
+  if (tryPoolName.reverted) {
+    log.info('pool name was reverted in tx hash: {} at blockNumber: {}', [
+      event.transaction.hash.toHex(),
+      event.block.number.toString(),
+    ]);
+  } else {
+    pool.name = tryPoolName.value;
+  }
+
+  pool.manager = managerContract.manager();
+  pool.managerName = poolContract.managerName();
+  pool.decimals = poolTokenDecimals;
+
+  let poolSupply = convertTokenToDecimal(
+    poolContract.totalSupply(),
+    poolTokenDecimals
+  );
+  pool.totalSupply = poolSupply;
+
+  let tryPoolTokenPrice = poolContract.try_tokenPrice();
+  if (tryPoolTokenPrice.reverted) {
+    log.info(
+      'pool token price was reverted in tx hash: {} at blockNumber: {}',
+      [event.transaction.hash.toHex(), event.block.number.toString()]
+    );
+  } else {
+    pool.tokenPrice = tryPoolTokenPrice.value;
+  }
+
+  // if (pool === null) log.error('pool is null', [pool.id]);
+  return pool as Pool;
+}
